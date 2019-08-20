2d21e669a60ea2b39e5aaef0b72e291b93206a93
GitHubForDmitry <dmitrykoka@gmail.com>
2019-08-20 22:58:33 +0300

    Updates

---
+++ precache-manifest.dfd0fc129ec36209954478c86702e56c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d23d18d093aa616e2c57efc66bab55f",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "148dbfccf63e6e5aa740",
+    "url": "/react_uber-eats/static/css/main.7d2a2b38.chunk.css"
+  },
+  {
+    "revision": "cb80db5dc7389401cb11",
+    "url": "/react_uber-eats/static/js/2.f38186e7.chunk.js"
+  },
+  {
+    "revision": "148dbfccf63e6e5aa740",
+    "url": "/react_uber-eats/static/js/main.96ee5696.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
